30e4587b1005a0c870e3c35bcb3ce3dc1f048c36
vasudeva999 <vasudevamanikanta999@gmail.com>
2021-08-18 21:34:29 +0530

    Requirement 1: Calculated Area of Rectangle with all possible conditions

--- src/main/java/RectangleAndSquare.java
+++ src/main/java/RectangleAndSquare.java
@@ -1,2 +1,3 @@
 public class RectangleAndSquare {
+    public int AreaOfRectangle(int length, int breadth){return (length>0 && breadth>0)?length*breadth:0;}
 }
--- src/test/java/RectangleAndSquareTest.java
+++ src/test/java/RectangleAndSquareTest.java
@@ -1,2 +1,33 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class RectangleAndSquareTest {
+    private RectangleAndSquare rectangleAndSquare;
+
+    @BeforeEach
+    public void createObject(){rectangleAndSquare = new RectangleAndSquare();}
+
+    @Nested
+    public class AreaOfRectangle {
+        @Test
+        public void toCalculateAreaWhenLengthAndBreadthArePositive() {
+            assertEquals(25, rectangleAndSquare.AreaOfRectangle(5, 5));
+        }
+
+        @Test
+        public void toCalculateAreaWhenLengthAndBreadthAreNegative() {
+            assertEquals(0, rectangleAndSquare.AreaOfRectangle(-5, -5));
+        }
+
+        @Test
+        public void toCalculateAreaWhenLengthIsPositiveAndBreadthIsNegative() {
+            assertEquals(0, rectangleAndSquare.AreaOfRectangle(5, -5));
+        }
+
+        @Test
+        public void toCalculateAreaWhenLengthIsNegativeAndBreadthIsPositive() {
+            assertEquals(0, rectangleAndSquare.AreaOfRectangle(-5, 5));
+        }
+    }
+
 }
