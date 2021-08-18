89e5362c5ea587aeaa65c224de1eb102263be771
vasudeva999 <vasudevamanikanta999@gmail.com>
2021-08-18 22:06:09 +0530

    Requirement 4: Calculated Perimeter of Square with all possible conditions

--- src/main/java/RectangleAndSquare.java
+++ src/main/java/RectangleAndSquare.java
@@ -3,5 +3,5 @@ public class RectangleAndSquare {
     public int perimeterOfRectangle(int length, int breadth){return (length>0 && breadth>0)?2*(length+breadth):0;}
 
     public int areaOfSquare(int side){return (side>0)?side*side:0;}
-    public int perimeterOfSquare(int side){return (side>0)?4*side:0;}
+    public int perimeterOfSquare(int side){return (side>0)?4*side:0; }
 }
